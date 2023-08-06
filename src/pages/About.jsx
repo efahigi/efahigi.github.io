639a7f1a64203f7443a2470eb5df41d4d657a680
Efa Higi <efawakene2005@gmail.com>
2023-08-06 14:48:14 -0500

    17th commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,42 +2,40 @@ import React from 'react'
 
 function About() {
   return (
-    <div class ="about">
-    <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-      <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-          Hi, I'm Reed.
-          <br className="hidden lg:inline-block" />I love to build amazing
-          apps.
-        </h1>
-        <p className="mb-8 leading-relaxed">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-          laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-          Laborum, voluptas natus?
-        </p>
-        <div className="flex justify-center">
-          <a
-            href="#contact"
-            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-            Work With Me
-          </a>
-          <a
-            href="#projects"
-            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-            See My Past Work
-          </a>
+    <div class ="aboutPage">
+      <div className="AboutContainer ">
+        <div className="myContainer">
+          <div className='myName'>
+            <h3>About Me</h3>  <br/>
+            <h5>I love to learn new knowlege and skill.</h5>
+          </div>
+          <div className="aboutMe">
+            <p >Software Engineer trained in Front End Web development, back-End Web development and Mobile application development with a skill set in HTML/CSS, Express, MongoDB, JavaScript (mainly, React, NodeJS) Swift and Objective C.  Experienced in tutoring students and guiding folks into becoming a better version of themselves. A patient and persistent professional with a strong analytical mindset complimented with critical thinking abilities to stay focused while exercising a strong attention to detail. </p>
+          </div>
+
+          <div className="aboutConnection">
+
+            <div className="contact">
+              <a href="#contact">
+              Contact Me
+              </a>
+            </div>
+
+            <div className="project">
+              <a href="#projects">
+                 My Work
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="myInformation">
+          {/* <img  src="./coding.svg"
+            className="imageInfo" 
+          /> */}
         </div>
       </div>
-      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-        <img
-          className="object-cover object-center rounded"
-          alt="hero"
-          src="./coding.svg"
-        />
-      </div>
     </div>
-</div>
-);
+  );
 }
 
 export default About
