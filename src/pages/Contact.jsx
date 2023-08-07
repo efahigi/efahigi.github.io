b01b6a242ea010ef44004a0a313000b3610c3bb9
Efa Higi <efawakene2005@gmail.com>
2023-08-07 07:39:51 -0500

    contact commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function Contact() {
   return (
     <div className='contactPage'id="contact">
-
       <div className="map">
             <iframe
             width="100%"
@@ -28,73 +27,12 @@ function Contact() {
               email: <span>efawakene2005@email.com</span>
               <a className="emailAddress"></a>
             </h5> 
-            
             <h6 className="phoneAddress">
-              Phone:<span>4694076748</span>
+              Phone:<span>+4694076748</span>
             </h6>
           </div>
-          
         </div>
-
-      
-      
-
-
-
-
-        {/* <form
-          netlify
-          name=""
-          className="">
-          <h2 className="">
-            Hire Me
-          </h2>
-          <p className="">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
-          </p>
-          <div className="">
-            <label htmlFor="name" className="">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="">
-            <label htmlFor="email" className="">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="">
-            <label
-              htmlFor="message"
-              className="">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Submit
-          </button>
-        </form> */}
       </div>
-   
   );
 }
 export default Contact
